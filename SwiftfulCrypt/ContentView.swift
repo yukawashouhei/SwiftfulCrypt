d77dff37041a2246c1ee00d89390c55a73109f9f
Shohei <yukawa.shouhei@gmail.com>
2025-07-25 23:12:08 +0900

    Initial Commit

---
+++ SwiftfulCrypt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulCrypt
+//
+//  Created by 湯川昇平 on 2025/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulCrypt/SwiftfulCryptApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptApp.swift
+//  SwiftfulCrypt
+//
+//  Created by 湯川昇平 on 2025/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
